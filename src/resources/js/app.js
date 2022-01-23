c68037af11ef195a027d7a5ca45a1d8f44fc15ea
Lucas Martins <mts.lucasmartins@gmail.com>
2022-01-23 18:37:53 -0300

    feat: created queries for firebase

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -25,16 +25,16 @@ class WebSocketClient {
   connect() {
     this.socket = new WebSocket(this.uri);
 
-    socket.onopen = (e) => {
+    this.socket.onopen = (e) => {
       const message = { type: 'open', message: 'connection stablished' };
       this.keepAlive();
     };
 
-    socket.onmessage = (event) => {
+    this.socket.onmessage = (event) => {
       const message = { type: 'message', message: event.data };
     };
 
-    socket.onclose = (event) => {
+    this.socket.onclose = (event) => {
       if (event.wasClean) {
         const message = { type: 'close', message: 'connection closed', wasClean: true };
       } else {
@@ -42,7 +42,7 @@ class WebSocketClient {
       }
     };
 
-    socket.onerror = (error) => {
+    this.socket.onerror = (error) => {
       const message = { type: 'error', message: error.message };
     };
 
