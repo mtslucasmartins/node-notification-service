9ee817308ba91326df561a08335665648ffff4db
Lucas Martins <mts.lucasmartins@gmail.com>
2022-02-10 20:23:06 -0300

    chore: minor adjustments

--- src/workers.js
+++ src/workers.js
@@ -20,10 +20,11 @@ class InstancePrunerWorker {
         const currentTime = new Date();
 
         for (const instanceId of instances) {
-          const instance = await this.instanceService.get(instanceId);
-          console.log(`[instance-pruner-worker] checking instance - instance:[${instanceId}]`, instance);
-          // check if instance.updatedAt is recent (15 seconds)
-          // if it is, great - if it's not, delete that key.
+          this.instanceService.get(instanceId).then((instance) => {
+            console.log(`[instance-pruner-worker] checking instance - instance:[${instanceId}]`, instance);
+            // check if instance.updatedAt is recent (15 seconds)
+            // if it is, great - if it's not, delete that key.
+          });
         }
       }).catch((error) => {
         console.log(`[instance-pruner-worker] error pruning instances`, error);
